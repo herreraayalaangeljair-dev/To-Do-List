51b191dc08e70ab0f86a559225e32a7c1a028277
Jair <angeljair354@gmail.com>
2026-08-09 17:32:03 -0600

    Tareas agregadas

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
+import { useState } from "react";
 import Header from "./Components/Header";
 import FormularioTareas from "./Components/FormularioTareas";
 
 const App = () => {
+    const [tareas, setTareas] = useState([]);
     return (
         <div className="container">
             <div className="to-do-list-container">
                 <div className="header-container">
                     <Header />
                 </div>
-                <FormularioTareas />
+                <FormularioTareas tareas={tareas} setTareas={setTareas} />
+                {tareas.map((tarea) => {
+                    return (<div className="task-container" key={tarea.id}>
+                        <p className="task-printed">{tarea.texto}</p>
+                        <input type="checkbox" className="check-input" />
+                    </div>);
+                })}
             </div>
             <footer className="footer">
                 <p className="footer_text">Angel Herrera © 2026</p>
--- src/Components/FormularioTareas.js
+++ src/Components/FormularioTareas.js
@@ -1,14 +1,40 @@
+import { useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
-const FormularioTareas = () => {
+
+const FormularioTareas = ({ tareas, setTareas }) => {
+    const [input, setInput] = useState('');
+
+    const handleInput = (e) => {
+        setInput(e.target.value);
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        setTareas(
+            [
+                ...tareas,
+                {
+                    id: uuidv4(),
+                    texto: input,
+                    completada: false
+                }
+            ]
+        )
+    }
 
     return (
         <div>
-            <form className="Formulario-Tareas">
+            <form className="Formulario-Tareas" onSubmit={handleSubmit}>
                 <input
                     type="text"
                     className="Formulario-Tareas-input"
-                    placeholder="Write a task"></input>
+                    placeholder="Write a task"
+                    value={input}
+                    onChange={handleInput}
+                    required
+                ></input>
                 <button
                     type="submit"
                     className="Formulario-Tareas-btn">
@@ -19,4 +45,4 @@ const FormularioTareas = () => {
     )
 }
 
-export default FormularioTareas 
+export default FormularioTareas; 
